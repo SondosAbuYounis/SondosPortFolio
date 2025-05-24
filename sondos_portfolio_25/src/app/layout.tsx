c9d01f89a59617fd21cb5af6a1910b07221e6962
SondosAbuYounis <abuyounissondos@gmail.com>
2025-05-25 02:08:27 +0300

    configured theme and color palette

--- sondos_portfolio_25/src/app/layout.tsx
+++ sondos_portfolio_25/src/app/layout.tsx
@@ -1,21 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Sondos AbuYounis",
   description: "Portfolio",
-  authors: [{ name: "Sondos AbuYounis", url: "https://www.linkedin.com/in/sundosabuyounes" }],
+  authors: [
+    {
+      name: "Sondos AbuYounis",
+      url: "https://www.linkedin.com/in/sundosabuyounes",
+    },
+  ],
   keywords: "portfolio",
 };
 
@@ -26,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`font-mainFontFamily antialiased`}>{children}</body>
     </html>
   );
 }
