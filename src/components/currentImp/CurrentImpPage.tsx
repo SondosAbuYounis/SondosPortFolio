e9dc69ec34314d6cb9f51af3c13fc1717a3ab378
SondosAbuYounis <abuyounissondos@gmail.com>
2025-06-03 20:47:46 +0300

    started implementing current page view

---
+++ src/components/currentImp/CurrentImpPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { HeadingFour, SubHeading } from "../typography";
+
+function CurrentImpPage() {
+  return (
+    <div className="flex flex-row">
+      <HeadingFour> Hi</HeadingFour>
+      <SubHeading>Welcome</SubHeading>
+    </div>
+  );
+}
+
+export default CurrentImpPage;
